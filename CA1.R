c25e821173db9a4506824c3098c563e817f80bae
Edgard Pacheco <72029558+edpachecopro@users.noreply.github.com>
2023-11-30 20:49:06 +0000

    TASK A Done

--- CA1.R
+++ CA1.R
@@ -5,10 +5,21 @@
 #install.packages("tidyr")
 #install.packages("ggplot2")
 #install.packages("DT")
+#install.packages("caret")
 
+
+# Load necessary libraries
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(DT)
+library(caret)
 
 # Load the dataset
-data <- read_csv("/Users/edgardpacheco/Library/CloudStorage/GoogleDrive-edgardpsilva@gmail.com/My Drive/01-CCT/Year-4/03 Tuesday - Mohamad - DataAnali/CA1/Crimes-2017.csv")
+data_full <- read_csv("Crimes-2017.csv")
+data_full <- read_csv("Crimes-2017-full.csv")
+
 
 
 # Print the first 10 lines
@@ -16,7 +27,35 @@ datatable(head(data, 8000))
 
 
 # Check variable types
+
+# Check variable types
 cat_vars <- c("ID", "Case Number", "Primary Type", "Description", "Location Description", "Arrest", "Domestic", "FBI Code")
 discrete_vars <- c("Beat", "District", "Ward", "Community Area", "Year")
 continuous_vars <- c("IUCR")
+
+# Visualize missing values
+missing_values <- data %>%
+  summarise_all(~ sum(is.na(.))) %>%
+  gather(variable, count)
+
+ggplot(missing_values, aes(x = variable, y = count)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(title = "Missing Values by Variable", x = "Variable", y = "Count") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+
+
+# Cleaning Data
+
+# Check for missing values and remove rows with any missing value
+data <- data_full %>%
+  na.omit()
+
+# Display the number of rows and columns before and after removing missing values
+cat("Before removing missing values:", nrow(data_full), "rows\n")
+cat("After removing missing values:", nrow(data), "rows\n")
+
+
 
