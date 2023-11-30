2e39d61d7c13bf9996b3be968268a4b5f3f1e13d
Edgard Pacheco <72029558+edpachecopro@users.noreply.github.com>
2023-11-30 19:01:37 +0000

    Inicial Commit

---
+++ CA1.R
@@ -0,0 +1,9 @@
+
+# Load the dataset
+data <- read_csv("/Users/edgardpacheco/Library/CloudStorage/GoogleDrive-edgardpsilva@gmail.com/My Drive/01-CCT/Year-4/03 Tuesday - Mohamad - DataAnali/CA1/Crimes-2017.csv")
+
+
+# Print the first 10 lines
+datatable(head(data, 8000))
+  
+ 
